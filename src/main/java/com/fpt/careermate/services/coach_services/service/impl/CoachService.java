4f33bcf5d306b76ed38c4a682ead170cdce773f0
chinhnd23 <chinhnd23@fpt.com>
2025-10-30 17:01:08 +0700

    refactore(coach): switch category to tags in course

--- src/main/java/com/fpt/careermate/services/coach_services/service/impl/CoachService.java
+++ src/main/java/com/fpt/careermate/services/coach_services/service/impl/CoachService.java
@@ -2,6 +2,7 @@ package com.fpt.careermate.services.coach_services.service.impl;
 
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fpt.careermate.services.coach_services.service.dto.request.CourseCreationRequest;
 import com.fpt.careermate.services.coach_services.service.dto.response.CourseListResponse;
 import com.fpt.careermate.services.coach_services.service.dto.response.CourseResponse;
 import com.fpt.careermate.services.coach_services.service.dto.response.QuestionResponse;
@@ -9,7 +10,7 @@ import com.fpt.careermate.services.coach_services.service.dto.response.QuestionR
 import java.util.List;
 
 public interface CoachService {
-    CourseResponse generateCourse(String topic);
+    CourseResponse generateCourse(CourseCreationRequest request);
     String generateLesson(int lessonId) throws JsonProcessingException;
     List<CourseListResponse> getMyCourses();
     void markLesson(int lessonId, boolean marked);
